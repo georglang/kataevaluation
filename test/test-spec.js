d0365c496e0e73897b81f6728d1022e65a3141ae
Georg Lang <georglang0788@gmx.net>
2014-01-13 21:21:08 +0100

    imp3

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,22 +1,28 @@
 /*
-I 1
-V 5
-X 10
-L 50
-C 100
-D 500
-M 1000
-*/
+ I 1
+ V 5
+ X 10
+ L 50
+ C 100
+ D 500
+ M 1000
+ */
 
-var convert = function () {
-  return 'I';
+var convert = function (arabicNum) {
+  if (arabicNum == 1) {
+    return 'I';
+  }
+  else{
+    return 'V';
+  }
 };
 
-describe('Roman Numberals', function() {
-  it('should convert 1 to I', function(){
+
+describe('Roman Numberals', function () {
+  it('should convert 1 to I', function () {
     expect(convert(1)).toBe('I');
   });
-  it('should convert 5 to V', function(){
+  it('should convert 5 to V', function () {
     expect(convert(5)).toBe('V');
   });
 });
