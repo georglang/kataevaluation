1c84ed268a06187fd4437b234cde0ac50f06560e
Georg Lang <georglang0788@gmx.net>
2014-01-14 08:52:08 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,5 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
+describe('Roman Numerals Session 2', function() {
+  it('should convert 1 to I', function(){
+    expect(convert(1)).toBe('I');
   });
 });
