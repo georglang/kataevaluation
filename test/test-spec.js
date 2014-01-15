17081db10c3a4ddb88e68da316c6ae7592fbd978
Georg Lang <georglang0788@gmx.net>
2014-01-15 15:32:24 +0100

    imp

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,10 +1,15 @@
-var convert = function(){ return 'I' };
+var convert = function (arabic) {
+  if (arabic == 1) {
+    return 'I'
+  }
+  return 'II'
+};
 
-describe('roman numerals session 3', function() {
-  it('I', function(){
+describe('roman numerals session 3', function () {
+  it('I', function () {
     expect(convert(1)).toBe('I');
   });
-  it('II', function(){
+  it('II', function () {
     expect(convert(2)).toBe('II');
   });
 });
