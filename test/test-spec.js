5c177fdb44ae38c84ee76848aabc19bbfa4f199b
Georg Lang <georglang0788@gmx.net>
2014-01-15 15:42:45 +0100

    #refactor

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,14 +1,11 @@
 var convert = function (arabic) {
-  if (arabic == 1) {
-    return 'I'
-  }
-  if (arabic == 2) {
-    return 'II'
-  }
-  if (arabic == 3) {
-    return 'III'
-  }
-  return 'IV'
+  var map = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV'
+  };
+  return map[arabic]
 };
 
 describe('roman numerals session 3', function () {
