c3278fa6b139e4eae075857e1073f347749db9c9
Georg Lang <georglang0788@gmx.net>
2014-01-15 14:45:33 +0100

    imp

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,11 +1,11 @@
-var arabicDigits = [10, 1];
-var romanDigits = ['X', 'I'];
+var arabicDigits = [10, 5, 1];
+var romanDigits = ['X', 'V', 'I'];
 
 var convert = function (arabic) {
   var roman = "";
 
-  for (var i=0; i<arabicDigits.length; i++){
-    while(arabic >= arabicDigits[i]){
+  for (var i = 0; i < arabicDigits.length; i++) {
+    while (arabic >= arabicDigits[i]) {
       roman += romanDigits[i];
       arabic -= arabicDigits[i];
     }
