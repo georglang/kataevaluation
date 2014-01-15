4bb1def7d8fcde324260d81570d4f9302e9de082
Georg Lang <georglang0788@gmx.net>
2014-01-15 14:08:15 +0100

    refactor

--- test/test-spec.js
+++ test/test-spec.js
@@ -5,27 +5,27 @@ var convert = function (arabic) {
     roman += 'X';
     arabic -= 10;
   }
-
-  while(arabic-- > 0){
-    roman += 'I'
+  while (arabic >= 1) {
+    roman += 'I';
+    arabic -= 1;
   }
   return roman;
 };
 
-describe('Roman Numerals Session 2', function() {
-  it('I', function(){
+describe('Roman Numerals Session 2', function () {
+  it('I', function () {
     expect(convert(1)).toBe('I');
   });
-  it('II', function(){
+  it('II', function () {
     expect(convert(2)).toBe('II');
   });
-  it('III', function(){
+  it('III', function () {
     expect(convert(3)).toBe('III');
   });
-  it('X', function(){
+  it('X', function () {
     expect(convert(10)).toBe('X');
   });
-  it('XX', function(){
+  it('XX', function () {
     expect(convert(20)).toBe('XX');
   });
 });
