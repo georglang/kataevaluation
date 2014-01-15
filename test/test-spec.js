8809dd91ab3d74c355b94f4d83c399223aefc00f
Georg Lang <georglang0788@gmx.net>
2014-01-15 15:31:32 +0100

    test2

--- test/test-spec.js
+++ test/test-spec.js
@@ -4,4 +4,7 @@ describe('roman numerals session 3', function() {
   it('I', function(){
     expect(convert(1)).toBe('I');
   });
+  it('II', function(){
+    expect(convert(2)).toBe('II');
+  });
 });
