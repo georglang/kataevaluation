43ba582d281a6d5676ec9db69ba6db60ab02f4ed
Georg Lang <georglang0788@gmx.net>
2014-01-23 19:03:23 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -6,4 +6,7 @@ describe('fizzBuzzSession2', function() {
   it('3 is fizz and 5 is buzz', function(){
     expect(fizzBuzz(15)).toBe("fizzbuzz");
   });
+  it('3 is fizz', function(){
+    expect(fizzBuzz(21)).toBe("fizz");
+  });
 });
