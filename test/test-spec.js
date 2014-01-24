e5af798846c6bc9fb018497100d556b80bde278e
Georg Lang <georglang0788@gmx.net>
2014-01-24 16:03:10 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -7,4 +7,7 @@ describe('wordWrapImpass', function() {
   it('wrap null returns empty string', function(){
     expect(wrap(null, 10)).toBe('');
   });
+  it('one short word does not wrap', function(){
+    expect(wrap('word', 5)).toBe('word');
+  });
 });
