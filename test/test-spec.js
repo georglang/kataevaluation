3788fa7c0009f90f0ba11b3d30aa7bd7e986882b
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 16:51:52 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,10 +1,13 @@
 describe('Game of Life - Session 5', function() {
   it("countNeighbours(i,j, matrix) returned a number between 0 and 8", function(){
-    expect(countNeighbours()).toBeGreaterThan(-1);
-    expect(countNeighbours()).toBeLessThan(9);
+    var matrix = [
+       [0,0,0], [0,0,0]
+    ];
+    expect(countNeighbours(1,2,matrix)).toBeGreaterThan(-1);
+    expect(countNeighbours(1,2, matrix)).toBeLessThan(9);
   });
 });
 
-var countNeighbours = function () {
+var countNeighbours = function (i,j,matrix) {
   return 0;
 }
