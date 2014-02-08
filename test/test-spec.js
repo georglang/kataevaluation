4b950ffab3823d1c765412d9554b39b67b0db517
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 10:24:05 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -12,4 +12,14 @@ describe('Game Of Life - Session 1', function() {
 
     expect(isCellLivingInNextStep(world, 1, 1)).toBe(false);
   });
+
+  it('Living cell with two or three live neighbors lives', function(){
+    var world = [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ];
+
+    expect(isCellLivingInNextStep(world, 1, 1)).toBe(true);
+  });
 });
