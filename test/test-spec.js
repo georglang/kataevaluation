b4543b7ac1603ba91f07f263f0c15761e24d6791
Marco Jantke <marco.jantke@mayflower.de>
2014-02-08 11:30:50 +0100

    first test impl

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,11 +1,46 @@
-var Game = function () {
-    var length = 3;
-    var height = 3;
-    this.grid = [[0,0,0],[0,1,0],[0,0,0]];
+var Game = function(length, height) {
+
+    this.grid = [];
+
+    for (var x = 0; x < length; x += 1) {
+        this.grid[x] = [];
+        for (var y = 0; y < height; y +=1 ) {
+            this.grid[x][y] = 0;
+        }
+    }
 }
 
-Game.prototype.getGrid = function () {
-  return this.grid;
+Game.prototype.getGrid = function() {
+    return this.grid;
+}
+
+Game.prototype.countActiveNeighbours = function(x, y) {
+    var topLeftNeighbour = this.grid[x - 1][y - 1];
+    var topNeighbour = this.grid[x][y - 1];
+    var topRightNeighbour = this.grid[x + 1][y - 1];
+
+    var leftNeighbour = this.grid[x][y - 1];
+    var rightNeighbour = this.grid[x][y + 1];
+
+    var bottomLeftNeighbour = this.grid[x - 1][y + 1];
+    var bottomNeighbour = this.grid[x][y + 1];
+    var bottomRightNeighbour = this.grid[x + 1][y + 1];
+
+    var neighbours = [
+        topLeftNeighbour, topNeighbour, topRightNeighbour,
+        leftNeighbour, rightNeighbour,
+        bottomLeftNeighbour, bottomNeighbour, bottomRightNeighbour
+    ];
+
+    var activeCount = 0;
+
+    for(var i = 0; i < neighbours.length; i += 1) {
+        if(neighbours[i] === 1) {
+            activeCount += 1;
+        }
+    }
+
+    return activeCount;
 }
 
 describe('Game of life', function() {
@@ -16,7 +51,12 @@ describe('Game of life', function() {
         expect(game.getGrid().length).toBe(3);
     });
 
-
+    it('should count the number of active neighbours of cell x,y', function() {
+        var game = new Game(3, 3);
+        var x = 1;
+        var y = 1;
+        expect(game.countActiveNeighbours(x, y)).toBe(0);
+    });
 
 });
 
