b33cfcadf304529325fb3e027153249d5d22989a
Marco Jantke <marco.jantke@mayflower.de>
2014-02-08 10:07:38 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,22 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
-  });
+describe('GameOfLife', function() {
+
+    var grid;
+
+    beforeEach(function() {
+        grid = new Grid();
+    });
+
+    it('should kill living cell with less than to neighbours', function() {
+        var cell = grid.get(2, 2);
+        cell.live();
+
+        var cell2 = grid.get(3, 3);
+        cell2.live();
+
+        grid.tick();
+
+        expect(cell.isDead()).toBeTruthy();
+        expect(cell2.isDead()).toBeTruthy();
+    });
+
 });
