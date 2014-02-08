14d4f820f9dbe5e533334452b31bfb1da3384639
Nikolai Onken <onken@uxebu.com>
2014-02-08 16:47:56 +0100

    Implement

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,7 +1,15 @@
+Coordinate = function() {
+
+};
+
+Coordinate.prototype.create = function() {
+  return [0,0];
+}
+
 
 describe('game of life', function() {
   it('Coordinate create returns an array', function() {
-    expect(new Coordinate.create())
-      .toBe([0, 0]);
+    expect((new Coordinate).create())
+      .toEqual([0, 0]);
   });
 });
