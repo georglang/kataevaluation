bd9a1fc0403ab67d60adfbd723ce8a81b233d0e1
Marco Jantke <marco.jantke@mayflower.de>
2014-02-08 10:48:51 +0100

    impl

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,3 +1,92 @@
+var Cell = function(isAlive) {
+    this.isAlive = isAlive || false;
+};
+
+Cell.prototype.isAlive = function() {
+    return this.isAlive;
+};
+
+Cell.prototype.live = function() {
+    this.isAlive = true;
+};
+
+Cell.prototype.die = function() {
+    this.isAlive = false;
+};
+
+var Grid = function() {
+    this.grid = [
+        [new Cell(), new Cell(), new Cell()],
+        [new Cell(), new Cell(), new Cell()],
+        [new Cell(), new Cell(), new Cell()]
+    ];
+};
+
+Grid.prototype.get = function(x, y) {
+    return this.grid[x][y];
+};
+
+Grid.prototype.tick = function() {
+    var grid = [];
+
+    for(var x = 0; x < this.grid.length; x++) {
+        for(var y = 0; y < this.grid[x].length; y++) {
+            var currentCell = this.get(x, y);
+            var neighbours = this.getNeighbours(x, y);
+            var countLivingNeighbours = 0;
+
+            for(var i = 0; i < neighbours.length; i++) {
+                if(neighbours[i].isLiving()) {
+                    countLivingNeighbours++;
+                }
+            }
+
+            var cellForNewGrid = new Cell(currentCell.isLiving());
+
+            if(countLivingNeighbours < 2) {
+                cellForNewGrid.die();
+            }
+
+            grid[x][y] = cellForNewGrid;
+        }
+    }
+
+    this.grid = grid;
+};
+
+Grid.prototype.getNeighbours = function(x, y) {
+    var neighbours = [];
+
+    if(x > 0 && y > 0) {
+        neighbours.push(this.get(x - 1, y - 1));
+    }
+    if(y > 0) {
+        neighbours.push(this.get(x, y - 1));
+    }
+
+    if(x < this.grid.length - 1 && y > 0) {
+        neighbours.push(this.get(x + 1, y - 1));
+    }
+
+    if(x < this.grid.length - 1) {
+        neighbours.push(this.get(x + 1, y));
+    }
+
+    if(x > 0 && y < this.grid[x].length - 1) {
+        neighbours.push(this.get(x - 1, y + 1));
+    }
+
+    if(y < this.grid[x].length - 1) {
+        neighbours.push(this.get(x, y + 1));
+    }
+    if(x < this.grid.length && y < this.grid[x].length - 1) {
+        neighbours.push(this.get(x + 1, y + 1));
+    }
+
+    return neighbours;
+
+}
+
 describe('GameOfLife', function() {
 
     var grid;
@@ -7,16 +96,16 @@ describe('GameOfLife', function() {
     });
 
     it('should kill living cell with less than to neighbours', function() {
-        var cell = grid.get(2, 2);
+        var cell = grid.get(1, 1);
         cell.live();
 
-        var cell2 = grid.get(3, 3);
+        var cell2 = grid.get(2, 2);
         cell2.live();
 
         grid.tick();
 
-        expect(cell.isDead()).toBeTruthy();
-        expect(cell2.isDead()).toBeTruthy();
+        expect(grid.get(1, 1).isLiving()).toBeFalsy();
+        expect(grid.get(2, 2).isLiving()).toBeFalsy();
     });
 
 });
