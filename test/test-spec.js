fd698389863da04e535f687bfaa59fc41dff3cbc
Nikolai Onken <onken@uxebu.com>
2014-02-08 16:50:48 +0100

    Test

--- test/test-spec.js
+++ test/test-spec.js
@@ -6,10 +6,20 @@ Coordinate.prototype.create = function() {
   return [0,0];
 }
 
-
 describe('game of life', function() {
   it('Coordinate create returns an array', function() {
     expect((new Coordinate).create())
       .toEqual([0, 0]);
+  });
+});
+
+describe('world', function() {
+  it('create', function() {
+     expect((new World).create())
+       .toEqual([
+         [0, 3],
+         [2, 1],
+         [4, 10]
+       ]);
   });
 });
