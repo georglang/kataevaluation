6cbcd0e5e9360e52466f553b21012dd27a1d69a9
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 14:29:25 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,8 +1,14 @@
 describe("board", function(){
   it("should be able to add a cell", function() {
     var board  = new Board();
-    board.addLivingell(0,0);
+    board.addLivingCell(0,0);
     expect(board.livingCells.length).toBe(1);
+  })
+
+  it("should render the correct cell coordinates", function() {
+    var board  = new Board();
+    board.addLivingCell(0,0);
+    expect(board.livingCells[0].getX()).toBe(0);
   })
 });
 
