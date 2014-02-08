06e93f9e7869e38e872d79f77e953c1297cc8124
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 11:17:10 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,11 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
+describe('Game of Life - Session 2', function() {
+  it('Living cell with less than two neighbors dies', function(){
+    var world = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ];
+
+    expect(willCellBeAliveInNextStep(world, 1, 1)).toBe(false);
   });
 });
