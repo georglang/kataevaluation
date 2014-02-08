fcaf210f0e74d50556d0ca1a1cd337e197143317
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 14:24:49 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,7 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
-  });
+describe("board", function(){
+  it("should be able to add a cell", function() {
+    var board  = new Board();
+    board.addLivingell(0,0);
+    expect(board.livingCells.length).toBe(1);
+  })
 });
