a83f2e27e800445bfb3327c77b6db6fc11bcf093
Mike Schäkermann <mschaekermann.mmt-b2011@fh-salzburg.ac.at>
2014-02-08 11:26:55 +0100

    imp

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,3 +1,23 @@
+willCellBeAliveInNextStep = function(world, posX, posY) {
+  var countNeighbours = 0;
+  for(var x = posX-1; x < 3; x++){
+    for(var y = posY-1; y < 3; y++){
+      if(world[x][y] === 1 && posX != x && posY!= y){
+        countNeighbours++;
+
+      }
+    }
+  }
+
+  if(countNeighbours < 2){
+    return false;
+  }
+
+
+}
+
+
+
 describe('Game of Life - Session 2', function() {
   it('Living cell with less than two neighbors dies', function(){
     var world = [
