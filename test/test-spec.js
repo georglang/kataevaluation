722e5eba6e6c5bf19a757759e0742e0846ddd5ea
Marco Jantke <marco.jantke@mayflower.de>
2014-02-08 11:21:25 +0100

    first test impl

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,22 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
-  });
+var Game = function () {
+    var length = 3;
+    var height = 3;
+    this.grid = [[0,0,0],[0,1,0],[0,0,0]];
+}
+
+Game.prototype.getGrid = function () {
+  return this.grid;
+}
+
+describe('Game of life', function() {
+
+    it('should create gaming grid in passed dimensions', function() {
+        var game = new Game(3, 3);
+
+        expect(game.getGrid().length).toBe(3);
+    });
+
+
+
 });
+
