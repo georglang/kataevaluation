89c29713cacd27385a24c868ad00f9b27937879d
Stephan Bönnemann <stephan@boennemann.me>
2014-02-12 23:21:26 +0100

    imp

--- test/s4-spec.js
+++ test/s4-spec.js
@@ -15,3 +15,33 @@ describe('game of life', function(){
     expect(shouldKill(world1,1,1)).toBe(true);
   });
 });
+
+var world1 = [[0,0,0], [0,1,0], [0,0,1]];
+var world2 = [[0,0,0], [1,1,0], [0,1,1]];
+var world3 = [[1,1,1], [1,1,1], [1,1,1]];
+var world4 = [[0,0,0], [1,0,0], [0,1,1]];
+
+function shouldKill (world, x, y) {
+  var count = count_neighbors(world, x, y);
+  if (count < 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isAlive(world, x, y) {
+  return !!world[x][y];
+}
+
+function count_neighbors(world, x, y) {
+  var count = 0;
+  for (var i=x-1; i<=x+1; i++) {
+    for (var j=y-1; j<=y+1; j++) {
+        if(i !== x || y !== j) {
+          count+=world[i][j];
+        }
+    }
+  }
+  return count;
+}
