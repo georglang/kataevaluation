07c2010ed04e1b83cae4e5e6110306c7486bfffd
Stephan Bönnemann <stephan@boennemann.me>
2014-02-12 23:22:01 +0100

    imp

--- test/s5-spec.js
+++ test/s5-spec.js
@@ -25,3 +25,46 @@ describe('game of life', function(){
   })
 
 });
+
+var Game = (function(){
+  function Game (){
+    this.cells = [];
+  };
+
+  Game.prototype.addCell = function(x,y) {
+    this.cells.push([x,y]);
+  };
+
+  Game.prototype.removeCell = function(x,y) {
+    this.cells = this.cells.filter(function(cell){
+      return !(cell[0] === x && cell[1] === y);
+    });
+  };
+
+  Game.prototype.getAllPossibleNeighbours = function(x,y) {
+    return [
+      [x-1,y-1],
+      [x-1,y],
+      [x-1,y+1],
+      [x,y-1],
+      [x,y+1],
+      [x+1,y-1],
+      [x+1,y],
+      [x+1,y+1]
+    ];
+  };
+
+  Game.prototype.getAllAliveNeighbours = function (x,y) {
+    var neighbours = this.getAllPossibleNeighbours(x,y);
+    var cells = this.cells;
+    return neighbours.filter(function(neighbour){
+      for (var i = 0; i < cells.length; i++) {
+        if (neighbour[0] === cells[i][0] && neighbour[1] === cells[i][1]) {
+          return true;
+        }
+      }
+    });
+  }
+
+  return Game;
+})();
